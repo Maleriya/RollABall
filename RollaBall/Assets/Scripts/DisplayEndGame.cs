41acf3e8eba0d93c0900aa58244ff09ae7967f11
Maleriya <glen9234@yandex.ru>
2021-02-24 22:53:13 +0300

    comm HW_3

---
+++ RollaBall/Assets/Scripts/DisplayEndGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public sealed class DisplayEndGame
+    {
+        private Text _finishGameLabel;
+
+        public DisplayEndGame(Text finishGameLabel)
+        {
+            _finishGameLabel = finishGameLabel; 
+            _finishGameLabel.text = String.Empty;
+        }
+
+        public void GameOver(object o, CaughtPlayerEventArgs args)
+        {
+            if (o == null || args == null)
+                throw new Exception("Для вывода сообщения о кнце игры не хватает данных.");
+
+            _finishGameLabel.text = $"Вы проиграли. Вас убил {((GameObject)o).name} {args.Color} цвета";
+        }
+    }
+
+}
