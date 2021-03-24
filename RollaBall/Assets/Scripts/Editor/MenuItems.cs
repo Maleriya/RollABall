91e5bb2235cb1c0bb77970337bd2f9fc3d80cdce
Maleriya <glen9234@yandex.ru>
2021-03-24 17:56:10 +0300

    Comm_HW_07

---
+++ RollaBall/Assets/Scripts/Editor/MenuItems.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+namespace Assets.Scripts.Editor
+{
+    public class MenuItems
+    {
+        [MenuItem("LenkaMenu/Создать и расставить объекты по кругу")]
+        private static void MenuOption()
+        {
+            EditorWindow.GetWindow(typeof(MyWindow), false, "LenkaMenu");
+        }
+
+    }
+}
